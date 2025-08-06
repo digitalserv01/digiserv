8c08342b2c0067f3500aa25f8441fbfc1224b4d7
CAT TOWER <lapostecoliis@gmail.com>
2025-08-06 02:12:14 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,12 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'plus.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'source.unsplash.com',
         port: '',
         pathname: '/**',
